810d5935e1725be6eef265a2afac3ea8815623aa
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2020-11-17 21:46:19 +0000

    Serve a static index.html

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express();
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-	res.send("Hello World, from Express!");
+	res.sendFile("./public/index.html");
+})
+
+app.get('/tea', (req, res) => {
+	res.set("X-", "TeaTime");
+	res.status(418);
+	res.send('I like coffee!');
 })
 
 
