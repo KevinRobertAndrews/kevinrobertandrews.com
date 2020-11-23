99674c6fb97920d004c592075040f50a7d5558fc
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2020-11-22 22:02:04 -0500

    Many changes. Added express and am working on making a POST route

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express')
+const cors = require('cors');
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('./client/build'))
+
+app.use(cors());
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
-	res.sendFile("./public/index.html");
+	res.sendFile("./client/build/index.html");
 })
 
-app.get('/tea', (req, res) => {
-	res.set("X-", "TeaTime");
-	res.status(418);
-	res.send('I like coffee!');
+app.get('/post-test', (req, res) => {
+	res.send("Posted data...")
 })
 
+app.post('/post-test', (req, res) => {
+	const { name, address } = req.body
+	console.log(name, address)
+	res.status(200).send({name, address});
+})
 
-app.listen(3000, () => {
-	console.log("Server running on PORT 3000")
+const PORT = 3000;
+
+app.listen(PORT, () => {
+	console.log(`Listening on PORT ${PORT}...`);
 })
